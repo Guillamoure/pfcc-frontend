f85c6ec62d8359532c59d5d91f16dac92d580d64
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-06-04 11:50:35 -0400

    created home and classes routes, and made a fetch to the api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './container/home'
+import Classes from './container/classes'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/classes" component={Classes} />
+      </React.Fragment>
+    </Router>
   );
 }
 
