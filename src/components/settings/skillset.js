6d6e2bcce459c78c51ef7499561c205bf79b74e2
Jack <jhilscheriv@gmail.com>
2019-07-30 09:54:18 -0400

    user can create skilset

--- src/components/settings/skillset.js
+++ src/components/settings/skillset.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 
 class Skillset extends React.Component {
 
@@ -7,13 +9,38 @@ class Skillset extends React.Component {
     skills: {},
     chosenSkills: [],
     addSkillset: false,
-    name: ""
+    name: "",
+    activeSkillset: 0
   }
 
   componentDidMount() {
+    // (this.state.activeSkillset !== this.props.currentUser.skillset_id) && this.setState({activeSkillset: this.props.currentUser.skillset_id})
     fetch("http://localhost:3000/api/v1/skillsets")
     .then(r => r.json())
     .then(data => this.setState({skillsets: data}), this.fetchSkills())
+  }
+
+  renderSkillsetSubmit = (e) => {
+    e.preventDefault()
+    fetch('http://localhost:3000/api/v1/skillsets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        chosen_skills: this.state.chosenSkills
+      })
+    })
+    .then(r => r.json())
+    .then(data => {
+      if (!data.error){
+        this.setState({name: "", skillsets: {}, skills: {}, chosenSkills: [], addSkillset: false})
+      } else {
+        console.log(data.error)
+      }
+    })
   }
 
   fetchSkills = () => {
@@ -50,7 +77,8 @@ class Skillset extends React.Component {
         {skill.name}:
         <input type="checkbox" name={skill.name} isSelected={() => this.selectedSkill(skill.id)} onChange={() => this.renderSkillChange(skill.id)}/>
       </label>
-      </div>)
+      </div>
+    )
     })
   }
 
@@ -62,8 +90,46 @@ class Skillset extends React.Component {
         <input type="text" name="name" value={this.state.name} onChange={this.renderChange}/>
       </label>
         {this.renderSkills()}
-        <button>Submit</button>
+        <button onClick={this.renderSkillsetSubmit}>Submit</button>
       </span>
+    )
+  }
+
+  renderActiveSkillsetChange = (e) =>{
+    const choice = e.target.value
+    fetch('http://localhost:3000/api/v1/users/active_skillset', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        id: this.props.currentUser.id,
+        active_skillset: choice
+      })
+    })
+    .then(r => r.json())
+    .then(data => {
+      if (!data.error){
+        console.log(data)
+        this.setState({activeSkillset: data.user.active_skillset})
+      } else {
+        console.log(data.error)
+      }
+    })
+  }
+
+  renderActiveSkillset = () => {
+    return (
+      <div>
+        <label>
+          Active Skillset:
+          <select name="activeSkillset" value={this.state.activeSkillset} onChange={this.renderActiveSkillsetChange}>
+            <option value= "0" >Select Skillset</option>
+            {this.state.skillsets.map(ss => <option value={ss.id}>{ss.name}</option>)}
+          </select>
+        </label>
+      </div>
     )
   }
 
@@ -71,11 +137,19 @@ class Skillset extends React.Component {
     console.log("skillset data", this.state)
     return(
       <span>
-        {!this.state.addSkillset && <button onClick={this.renderAddSkillset}>+</button>}
+        {(!this.state.addSkillset && this.props.admin) && <button onClick={this.renderAddSkillset}>+</button>}
         {this.state.addSkillset && this.skillsetForm()}
+        {!!this.state.skillsets.keys && this.renderActiveSkillset()}
       </span>
     )
   }
 }
 
-export default Skillset
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.currentUser,
+    admin: state.admin
+  }
+}
+
+export default connect(mapStateToProps)(Skillset)
