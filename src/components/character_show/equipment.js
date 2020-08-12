4e4c25b9a0d6704825a08aa75caff376a1a5d39e
Jack Hilscher <jhilscheriv@gmail.com>
2020-08-11 20:31:22 -0400

    shifted equipment item to its own component

--- src/components/character_show/equipment.js
+++ src/components/character_show/equipment.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import localhost from '../../localhost'
 
 import MagicItemSummary from '../magic_item_summary'
+import EquipmentItem from './equipment_item'
 
 
 class Equipment extends React.Component {
@@ -36,27 +37,6 @@ class Equipment extends React.Component {
       }
       // this.setState({itemObject: obj, descriptionAvailable: false})
     }
-  }
-
-  remappedActions = (actions) => {
-    return actions.map(a => {
-      switch(a){
-        case 'Standard Action':
-          return 'standard'
-        case 'Swift Action':
-          return 'swift'
-        case 'Move Action':
-          return 'move'
-        case 'Full-Round Action':
-          return 'full'
-        case 'Immediate Action':
-          return 'immediate'
-        case 'Free Action':
-          return 'free'
-        default:
-          return a
-      }
-    })
   }
 
   renderEquipment = () => {
@@ -93,27 +73,8 @@ class Equipment extends React.Component {
           {chosen && <ul style={{margin: '0', padding: '0'}}>
             {groupedItems.map((i, idx) => {
 
-              let keyWord = group
-              keyWord = group === 'Wondrous Item' ? 'magic_item' : keyWord
+							return <EquipmentItem item={i} index={idx} group={group} changeSelectedItem={this.changeSelectedItem}/>
 
-              let known = group === 'unknown' ? false : true
-              let actions = known && i[keyWord].features ? i[keyWord].features.map(f => f.action ? f.action.name : null) : []
-              let name = known ? i.name || i[keyWord].name : i.false_desc
-              let percentages = known && i[keyWord].features ? this.renderPercentage(i[keyWord]) : null
-              // find feature usage, find limit, and find relevant cmifu, and have a fraction of current usage
-              let remappedActions = this.remappedActions(actions)
-              let equipped = i.equipped
-              let stored = this.isThisStored(i, keyWord)
-              let id = i.id
-
-              return (
-                <>
-                  <li className='noStyleLi' style={{fontSize: 'smaller'}} key={idx*i.id*3-1} onClick={() => this.changeSelectedItem(id, group, i)}>
-                    {name}{percentages}{equipped ? <span className='equipped'>{equipped[0]}</span> : null}{stored ? <span className='equipped'>S</span> : null}
-                    {!!actions.length && remappedActions.map((a, idx) => <span key={idx*3+1} className={a} style={{borderRadius: '100%', paddingLeft: '8%', margin: '1.5%'}}>{'  '}</span>)}
-                  </li>
-                </>
-              )
             })}
           </ul>}
         </div>
@@ -133,33 +94,6 @@ class Equipment extends React.Component {
     //     )
     //
     // })
-  }
-
-  renderPercentage = (magicItem) => {
-    let mi = magicItem.features.find(f => f.usage)
-    if (mi){
-      let usage = mi.usage
-      let limit = usage.limit
-      if ((limit === 1 && usage.destroy_after_use) || limit === 1000){
-        return null
-      }
-      let cmifus = this.props.character.character_magic_item_feature_usages
-      let fu = cmifus.find(fu => fu.feature_usage_id === usage.id) || 0
-      let currentUsage = fu.current_usage || 0
-      let remaining = limit - currentUsage
-      return ` (${remaining}/${limit})`
-    } else {
-      return null
-    }
-  }
-
-  isThisStored = (item, group) => {
-    if (group === 'magic_item'){
-      return item.stored_character_magic_item
-    }
-    // ADD NEW STORAGE METHODS FOR NEW ITEM WHEN YOU GET TO IT
-    // debugger
-    return null
   }
 
 
