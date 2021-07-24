b4318f957fcfbe24a35301d30ba68cbdbf63f7cd
Jack Hilscher <jhilscheriv@gmail.com>
2021-07-24 10:23:11 -0400

    Updates

---
+++ precache-manifest.07aa194766277988f9ea34073bb5f235.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0cefe5f2178c1192d7e78420f55327",
+    "url": "/pfcc-frontend/index.html"
+  },
+  {
+    "revision": "1a2c0bb7c1ecd1d8a8ce",
+    "url": "/pfcc-frontend/static/css/main.a75d58ca.chunk.css"
+  },
+  {
+    "revision": "46efb6dacded7d7532e0",
+    "url": "/pfcc-frontend/static/js/2.8f3d853a.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/pfcc-frontend/static/js/2.8f3d853a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2c0bb7c1ecd1d8a8ce",
+    "url": "/pfcc-frontend/static/js/main.48fa0f0d.chunk.js"
+  },
+  {
+    "revision": "51cd980521a8df620eb4",
+    "url": "/pfcc-frontend/static/js/runtime-main.35c945f9.js"
+  },
+  {
+    "revision": "14f6d8ebf99a384edc904ea97229b3df",
+    "url": "/pfcc-frontend/static/media/d10.14f6d8eb.svg"
+  },
+  {
+    "revision": "8f63d1963253a2c569f5cc28c1913bb4",
+    "url": "/pfcc-frontend/static/media/d12.8f63d196.svg"
+  },
+  {
+    "revision": "d4563f05cbbc7cafa93f95861fb5c577",
+    "url": "/pfcc-frontend/static/media/d20.d4563f05.svg"
+  },
+  {
+    "revision": "58e63734294896c327f5765396305248",
+    "url": "/pfcc-frontend/static/media/d3.58e63734.svg"
+  },
+  {
+    "revision": "0f472d02408ddb1e596a505dd3f85256",
+    "url": "/pfcc-frontend/static/media/d4.0f472d02.svg"
+  },
+  {
+    "revision": "a3dc0163b585113f60adbe4d26bb16a5",
+    "url": "/pfcc-frontend/static/media/d6.a3dc0163.svg"
+  },
+  {
+    "revision": "8b2ab8803713bc2c967c169e2a994610",
+    "url": "/pfcc-frontend/static/media/d8.8b2ab880.svg"
+  },
+  {
+    "revision": "3cf54354badc2bf94aa4226843b8e37d",
+    "url": "/pfcc-frontend/static/media/rolling-dice-cup.3cf54354.svg"
+  }
+]);
